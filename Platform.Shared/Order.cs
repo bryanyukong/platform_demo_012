1823119fb5bfbe8e3cb69d89a1687a32ce33bae6
bryan pogenio <bryanmiguelpogenio@gmail.com>
2023-09-02 12:57:23 +0800

    rename blank solution

---
+++ Platform.Shared/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace Platform.Shared
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Platform.Shared/PlatformContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Platform.Shared
+{
+    public class PlatformContext : DbContext
+    {
+        public PlatformContext(DbContextOptions<PlatformContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasKey(k => new { k.Id });
+            modelBuilder.Entity<Order>().HasKey(k => new { k.Id });
+        }
+
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Order> Order { get; set; }
+    }
+}
